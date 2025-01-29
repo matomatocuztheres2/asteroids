3b845fadf718a5e9802ff9e7d66f0572e99b960a
matomatocuztheres2 <kedtealeaf@gmail.com>
2025-01-29 13:50:12 -0600

    Initalized window to black screen

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#import pygame
+import pygame
+#import constants from contants file.
 from constants import *
 
+
 def main():
+	#check if pygame is initalized, if not initalize it
+	if pygame.get_init() != True:
+		pygame.init()
+	#print width and height
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	#draw screen surface
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen_color = (0,0,0)
+	game_run = 1
+	while game_run == 1:
+		screen.fill(screen_color)
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
